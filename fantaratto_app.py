559f5fa3ddd0710b2143a5a201cdef44279a2e3a
mccatta <albertocattaneo02@gmail.com>
2025-10-30 13:27:43 +0100

    Update fantaratto_app.py

--- fantaratto_app.py
+++ fantaratto_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-from supabase import create_client, Client
+from supabase import create_client
 import uuid
 
 # --- CONFIG ---
